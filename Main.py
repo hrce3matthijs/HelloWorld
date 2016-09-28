82b4833f56c61fc91bbf0b5467eb7010401dce9c
Jesper <129175@hondsrugcollege.nl>
2016-09-28 15:28:07 +0200

    Update main.py
    Kaas

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from User import User
 
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
 Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
+Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
 
 print(Jelte)
 print(Matthijs)
+print(Jesper)
