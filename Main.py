d11fb01e25bed5d28b681d2a29704da809099b81
Matthijs Kleine <matt01jeroen@gmail.com>
2016-09-28 15:34:13 +0200

    PLOX ADD ME SENPAI
    NOTICE ME SENPAI

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from User import User
 
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
-Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
+Matthijs = User("Matthijs", "9000", "129472@hondsrugcollege.nl")
 Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
 
 print(Jelte)
